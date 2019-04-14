a2fcbd5b11feb39a7c4c359c3ec4cd6c33388d8f
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-14 18:55:04 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,9 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kid_name, age|
+    puts "Happy Birthday #{kid_name}! You are now #{age} years old!"
+  end
 end
 
 
